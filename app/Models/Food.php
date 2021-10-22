a36df1646760a33083bf3741f829b36a9c61b2ba
Aliff <aliff.rosli96@gmail.com>
2021-10-22 23:22:20 +0800

    food model create

---
+++ app/Models/Food.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Food extends Model
+{
+    use HasFactory;
+
+    protected $table = 'foods'; 
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'price',
+        'price_promotion',
+    ];
+}
