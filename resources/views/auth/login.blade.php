2567126a5c9d32c7b5b6a213d72ad0a3b98a1136
Aliff <aliff.rosli96@gmail.com>
2021-10-22 22:04:12 +0800

    update login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -87,41 +87,21 @@
                 <div class="col-xl-8 col-lg-10 col-md-12 m-auto">
                     <div class="row">
                         <div class="col-lg-6 pr-30 d-none d-lg-block">
-                            <img class="border-radius-15" src="assets/imgs/page/login-1.png" alt="">
+                            <img class="border-radius-15" src="{!!asset('assets/imgs/logo.jpg')!!}" alt="">
                         </div>
                         <div class="col-lg-6 col-md-8">
                             <div class="login_wrap widget-taber-content background-white">
                                 <div class="padding_eight_all bg-white">
                                     <div class="heading_s1">
-                                        <h1 class="mb-5">Login</h1>                                    </div>
+                                        <h1 class="mb-5">Login</h1>                                    
+                                    </div>
                                     <form method="POST" action="{{ route('login') }}">
                                         @csrf
                                         <div class="form-group">
                                             <input id="email" type="email" placeholder="Username / Email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" >
-                                            @error('email')
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
                                         </div>
                                         <div class="form-group">
                                             <input id="password" type="password" placeholder="Password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                                            @error('password')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                        </div>
-                                        <div class="login_footer form-group">
-                                            <div class="chek-form">
-                                                <input type="text" name="email" placeholder="Security code *">
-                                            </div>
-                                            <span class="security-code">
-                                                <b class="text-new">8</b>
-                                                <b class="text-hot">6</b>
-                                                <b class="text-sale">7</b>
-                                                <b class="text-best">5</b>
-                                            </span>
                                         </div>
                                         <div class="login_footer form-group mb-50">
                                             <a class="text-muted" href="#">Forgot password?</a>
