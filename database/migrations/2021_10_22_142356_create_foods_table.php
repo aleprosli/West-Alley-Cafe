635ae76d05d3b55a36c4ee7bc00e1713d5412f9d
Aliff <aliff.rosli96@gmail.com>
2021-10-22 23:22:09 +0800

    food migration create

---
+++ database/migrations/2021_10_22_142356_create_foods_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('category_id')->nullable();
+            $table->string('name')->nullable();
+            $table->decimal('price',8,2)->nullable();
+            $table->decimal('price_promotion',8,2)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foods');
+    }
+}
