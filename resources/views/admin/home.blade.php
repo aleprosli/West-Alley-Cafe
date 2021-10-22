6acb4f23777e76336c4bd6719bf9d64e874120bd
Aliff <aliff.rosli96@gmail.com>
2021-10-22 22:21:06 +0800

    update blade for admin blade

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -22,10 +22,13 @@
                                     <a class="nav-link active" id="dashboard-tab" data-bs-toggle="tab" href="#dashboard" role="tab" aria-controls="dashboard" aria-selected="false"><i class="fi-rs-settings-sliders mr-10"></i>Dashboard</a>
                                 </li>
                                 <li class="nav-item">
+                                    <a class="nav-link" id="menumakanan-tab" data-bs-toggle="tab" href="#menu" role="tab" aria-controls="menu" aria-selected="false"><i class="fi-rs-shopping-bag mr-10"></i>Menu</a>
+                                </li>
+                                <li class="nav-item">
                                     <a class="nav-link" id="orders-tab" data-bs-toggle="tab" href="#orders" role="tab" aria-controls="orders" aria-selected="false"><i class="fi-rs-shopping-bag mr-10"></i>Orders</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" id="track-orders-tab" data-bs-toggle="tab" href="#track-orders" role="tab" aria-controls="track-orders" aria-selected="false"><i class="fi-rs-shopping-cart-check mr-10"></i>Track Your Order</a>
+                                    <a class="nav-link" id="track-orders-tab" data-bs-toggle="tab" href="#track-orders" role="tab" aria-controls="track-orders" aria-selected="false"><i class="fi-rs-shopping-cart-check mr-10"></i>Add Menu</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" id="address-tab" data-bs-toggle="tab" href="#address" role="tab" aria-controls="address" aria-selected="true"><i class="fi-rs-marker mr-10"></i>My Address</a>
@@ -34,7 +37,12 @@
                                     <a class="nav-link" id="account-detail-tab" data-bs-toggle="tab" href="#account-detail" role="tab" aria-controls="account-detail" aria-selected="true"><i class="fi-rs-user mr-10"></i>Account details</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="page-login.html"><i class="fi-rs-sign-out mr-10"></i>Logout</a>
+                                    <a class="nav-link" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                  document.getElementById('logout-form').submit();"><i class="fi-rs-sign-out mr-10"></i>Logout</a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
                                 </li>
                             </ul>
                         </div>
@@ -48,6 +56,51 @@
                                     </div>
                                     <div class="card-body">
                                         <p>From your account dashboard. you can easily check &amp; view your <a href="#">recent orders</a>,<br> manage your <a href="#">shipping and billing addresses</a> and <a href="#">edit your password and account details.</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="menu" role="tabpanel" aria-labelledby="menumakanan-tab">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h3 class="mb-0">Menu Makanan</h3>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="table-responsive">
+                                            <table class="table">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Order</th>
+                                                        <th>Date</th>
+                                                        <th>Status</th>
+                                                        <th>Total</th>
+                                                        <th>Actions</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>#1357</td>
+                                                        <td>March 45, 2020</td>
+                                                        <td>Processing</td>
+                                                        <td>$125.00 for 2 item</td>
+                                                        <td><a href="#" class="btn-small d-block">View</a></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>#2468</td>
+                                                        <td>June 29, 2020</td>
+                                                        <td>Completed</td>
+                                                        <td>$364.00 for 5 item</td>
+                                                        <td><a href="#" class="btn-small d-block">View</a></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>#2366</td>
+                                                        <td>August 02, 2020</td>
+                                                        <td>Completed</td>
+                                                        <td>$280.00 for 3 item</td>
+                                                        <td><a href="#" class="btn-small d-block">View</a></td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -99,7 +152,7 @@
                             <div class="tab-pane fade" id="track-orders" role="tabpanel" aria-labelledby="track-orders-tab">
                                 <div class="card">
                                     <div class="card-header">
-                                        <h3 class="mb-0">Orders tracking</h3>
+                                        <h3 class="mb-0">Tambah Menu</h3>
                                     </div>
                                     <div class="card-body contact-from-area">
                                         <p>To track your order please enter your OrderID in the box below and press "Track" button. This was given to you on your receipt and in the confirmation email you should have received.</p>
@@ -107,14 +160,22 @@
                                             <div class="col-lg-8">
                                                 <form class="contact-form-style mt-30 mb-50" action="#" method="post">
                                                     <div class="input-style mb-20">
-                                                        <label>Order ID</label>
-                                                        <input name="order-id" placeholder="Found in your order confirmation email" type="text">
+                                                        <label>Category</label>
+                                                        <input name="category" placeholder="Category" type="text">
                                                     </div>
                                                     <div class="input-style mb-20">
-                                                        <label>Billing email</label>
-                                                        <input name="billing-email" placeholder="Email you used during checkout" type="email">
+                                                        <label>Nama Makanan</label>
+                                                        <input name="name" placeholder="Nama Makanan" type="text">
                                                     </div>
-                                                    <button class="submit submit-auto-width" type="submit">Track</button>
+                                                    <div class="input-style mb-20">
+                                                        <label>Harga</label>
+                                                        <input name="price" placeholder="Harga" type="text">
+                                                    </div>
+                                                    <div class="input-style mb-20">
+                                                        <label>Harga Promosi</label>
+                                                        <input name="price_promotion" placeholder="Harga Promosi" type="text">
+                                                    </div>
+                                                    <button class="submit submit-auto-width" type="submit">Tambah Menu</button>
                                                 </form>
                                             </div>
                                         </div>
