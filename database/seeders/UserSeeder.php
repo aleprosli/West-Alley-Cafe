4d839cedf154c1b3a96ac43c052c4ce2d09557ad
Aliff <aliff.rosli96@gmail.com>
2021-10-22 22:02:18 +0800

    seeder for admin

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'id' => 1,
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('password'),
+                'is_admin' => 1,
+                'created_at' => now()
+            ],
+            [
+                'id' => 2,
+                'name' => 'User',
+                'email' => 'user@gmail.com',
+                'password' => bcrypt('password'),
+                'is_admin' => NULL,
+                'created_at' => now()
+            ]
+            ]);
+    }
+}
