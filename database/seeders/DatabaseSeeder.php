691241bcfee6bd0f5b1e6dbfc75c75345f8aeed6
Aliff <aliff.rosli96@gmail.com>
2021-10-24 20:17:56 +0800

    register category seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Database\Seeders\UserSeeder;
+use Database\Seeders\CategorySeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([UserSeeder::class]);
+        $this->call([CategorySeeder::class]);
+
 
     }
 }
