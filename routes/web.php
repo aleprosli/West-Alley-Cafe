cd98eaea1ecb6f71e2275ff5f29acc58792fa02a
Aliff <aliff.rosli96@gmail.com>
2021-10-24 20:19:32 +0800

    route updated

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,10 @@ Route::get('/', function () {
     $foods = Food::all();
 
     return view('user.home',compact('foods'));
-});
+})->name('/');
 
 Auth::routes();
 
 Route::get('/admin/home', [App\Http\Controllers\HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
 Route::post('/tambah-menu', [App\Http\Controllers\Admin\FoodController::class, 'store'])->name('menu:store');
+Route::get('/buang-menu', [App\Http\Controllers\Admin\FoodController::class, 'destroy'])->name('menu:destroy');
