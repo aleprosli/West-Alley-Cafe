8bf57c521d26029989694593a666e84d940bc78a
Aliff <aliff.rosli96@gmail.com>
2021-10-24 20:19:02 +0800

    update route / remove unnecessary part

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -195,7 +195,7 @@
                                 </div>
                                 @else
                                 <div class="header-action-icon-2">
-                                    <a class="mini-cart-icon" href="shop-cart.html">
+                                    <a class="mini-cart-icon" href="{{ route('admin.home') }}">
                                         <img class="svgInject" alt="Nest" src="{!!asset('assets/imgs/theme/icons/icon-user.svg')!!}">
                                     </a>
                                     <span class="lable">{{ Auth::user()->name }}</span>
@@ -226,108 +226,11 @@
                     <div class="logo logo-width-1 d-block d-lg-none">
                         <a href="index.html"><img src="assets/imgs/theme/logo.svg" alt="logo"></a>
                     </div>
-                    <div class="header-nav d-none d-lg-flex">
-                        <div class="main-categori-wrap d-none d-lg-block">
-                            <a class="categories-button-active " href="#">
-                                <span class="fi-rs-apps"></span> <span class="et">Browse</span> All Categories
-                                <i class="fi-rs-angle-down"></i>
-                            </a>
-                        </div>
-                        <div class="main-menu main-menu-padding-1 main-menu-lh-2 d-none d-lg-block font-heading">
-                            <nav>
-                                <ul>
-                                    <li class="hot-deals"><img src="{!! asset('assets/imgs/theme/icons/icon-hot.svg') !!}" alt="hot deals"><a href="shop-grid-right.html">Hot Deals</a></li>
-
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
-                    <div class="hotline d-none d-lg-flex">
-                        <img src="{!! asset('assets/imgs/theme/icons/phone-call.svg') !!}" height="40px" width="40px" alt="hotline">
-                        <i class="fa fa-mobile" aria-hidden="true"></i>
-                        <p>60 13-609 0205<span>West Alley Cafe H/P</span></p>
-                    </div>
                     <div class="header-action-icon-2 d-block d-lg-none">
                         <div class="burger-icon burger-icon-white">
                             <span class="burger-icon-top"></span>
                             <span class="burger-icon-mid"></span>
                             <span class="burger-icon-bottom"></span>
-                        </div>
-                    </div>
-                    <div class="header-action-right d-block d-lg-none">
-                        <div class="header-action-2">
-                            @guest
-                            <div class="header-action-icon-2">
-                                <a href="{{ route('login') }}">
-                                    <img class="svgInject" alt="Nest" src="{!!asset('assets/imgs/theme/icons/icon-user.svg')!!}">
-                                </a>
-                                <span class="lable ml-0">Account</span>
-                            </div>
-                            <div class="header-action-icon-2">
-                                <a class="mini-cart-icon" href="shop-cart.html">
-                                    <img alt="Nest" src="{!!asset('assets/imgs/theme/icons/icon-cart.svg')!!}">
-                                    <span class="pro-count blue">2</span>
-                                </a>
-                                <span class="lable">Cart</span>
-                                <div class="cart-dropdown-wrap cart-dropdown-hm2">
-                                    <ul>
-                                        <li>
-                                            <div class="shopping-cart-img">
-                                                <a href="shop-product-right.html"><img alt="Nest" src="{!!asset('assets/imgs/shop/thumbnail-3.jpg')!!}"></a>
-                                            </div>
-                                            <div class="shopping-cart-title">
-                                                <h4><a href="shop-product-right.html">Daisy Casual Bag</a></h4>
-                                                <h4><span>1 × </span>$800.00</h4>
-                                            </div>
-                                            <div class="shopping-cart-delete">
-                                                <a href="#"><i class="fi-rs-cross-small"></i></a>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="shopping-cart-img">
-                                                <a href="shop-product-right.html"><img alt="Nest" src="{!!asset('assets/imgs/shop/thumbnail-2.jpg')!!}"></a>
-                                            </div>
-                                            <div class="shopping-cart-title">
-                                                <h4><a href="shop-product-right.html">Corduroy Shirts</a></h4>
-                                                <h4><span>1 × </span>$3200.00</h4>
-                                            </div>
-                                            <div class="shopping-cart-delete">
-                                                <a href="#"><i class="fi-rs-cross-small"></i></a>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                    <div class="shopping-cart-footer">
-                                        <div class="shopping-cart-total">
-                                            <h4>Total <span>$4000.00</span></h4>
-                                        </div>
-                                        <div class="shopping-cart-button">
-                                            <a href="shop-cart.html" class="outline">View cart</a>
-                                            <a href="shop-checkout.html">Checkout</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            @else
-                            <div class="header-action-icon-2">
-                                <a class="mini-cart-icon" href="shop-cart.html">
-                                    <img class="svgInject" alt="Nest" src="{!!asset('assets/imgs/theme/icons/icon-user.svg')!!}">
-                                </a>
-                                <span class="lable">{{ Auth::user()->name }}</span>
-                                <div class="cart-dropdown-wrap cart-dropdown-hm2">
-                                    <ul>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                          document.getElementById('logout-form').submit();"><i class="fi-rs-sign-out mr-10"></i>Logout</a>
-                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                                @csrf
-                                            </form>
-                                        </li>
-                                        
-                                    </ul>
-                                </div>
-                            </div>
-                            @endguest
                         </div>
                     </div>
                 </div>
