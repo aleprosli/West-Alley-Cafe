b4fdadaae633b91d89ef30101883496cd7fac024
Aliff <aliff.rosli96@gmail.com>
2021-10-24 20:17:25 +0800

    relation hasOne added

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Food;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
@@ -12,4 +13,9 @@ class Category extends Model
     protected $fillable = [
         'type',
     ];
+
+    public function food()
+    {
+        return $this->hasOne(Food::class);
+    }
 }
