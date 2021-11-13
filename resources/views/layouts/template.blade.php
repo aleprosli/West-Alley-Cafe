5b0ebb597215fdf21a3291f5f7b1f1ae3f0fb23f
Aliff <aliff.rosli96@gmail.com>
2021-11-13 12:43:51 +0800

    session added

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -54,44 +54,40 @@
                                     <span class="lable ml-0">Account</span>
                                 </div>
                                 <div class="header-action-icon-2">
+                                    @php $total = 0 @endphp
+                                    @php $totalquantity = 0 @endphp
+                                    @foreach((array) session('cart') as $id => $details)
+                                        @php $total += $details['price'] * $details['quantity'] @endphp
+                                        @php $totalquantity += $details['quantity'] @endphp
+                                    @endforeach
                                     <a class="mini-cart-icon" href="shop-cart.html">
                                         <img alt="Nest" src="{!!asset('assets/imgs/theme/icons/icon-cart.svg')!!}">
-                                        <span class="pro-count blue">2</span>
+                                        <span class="pro-count blue">{{ $totalquantity }}</span>
                                     </a>
                                     <span class="lable">Cart</span>
+                                    
                                     <div class="cart-dropdown-wrap cart-dropdown-hm2">
-                                        <ul>
-                                            <li>
-                                                <div class="shopping-cart-img">
-                                                    <a href="shop-product-right.html"><img alt="Nest" src="{!!asset('assets/imgs/shop/thumbnail-3.jpg')!!}"></a>
-                                                </div>
-                                                <div class="shopping-cart-title">
-                                                    <h4><a href="shop-product-right.html">Daisy Casual Bag</a></h4>
-                                                    <h4><span>1 × </span>$800.00</h4>
-                                                </div>
-                                                <div class="shopping-cart-delete">
-                                                    <a href="#"><i class="fi-rs-cross-small"></i></a>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <div class="shopping-cart-img">
-                                                    <a href="shop-product-right.html"><img alt="Nest" src="{!!asset('assets/imgs/shop/thumbnail-2.jpg')!!}"></a>
-                                                </div>
-                                                <div class="shopping-cart-title">
-                                                    <h4><a href="shop-product-right.html">Corduroy Shirts</a></h4>
-                                                    <h4><span>1 × </span>$3200.00</h4>
-                                                </div>
-                                                <div class="shopping-cart-delete">
-                                                    <a href="#"><i class="fi-rs-cross-small"></i></a>
-                                                </div>
-                                            </li>
-                                        </ul>
+                                        @if(session('cart'))
+                                            @foreach(session('cart') as $id => $details)
+                                                <ul>
+                                                    <li>
+                                                        <div class="shopping-cart-img">
+                                                            <a href="shop-product-right.html"><img alt="Nest" src="{{ asset('/storage/image/'.$details['image']) }}"></a>
+                                                        </div>
+                                                        <div class="shopping-cart-title">
+                                                            <h4><a href="shop-product-right.html">{{ $details['name'] }}</a></h4>
+                                                            <h4><span>{{ $details['quantity'] }} × </span>{{ $details['price_promotion'] }}</h4>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            @endforeach
+                                        @endif
                                         <div class="shopping-cart-footer">
                                             <div class="shopping-cart-total">
-                                                <h4>Total <span>$4000.00</span></h4>
+                                                <h4>Total RM:<span>{{ $total }}</span></h4>
                                             </div>
                                             <div class="shopping-cart-button">
-                                                <a href="shop-cart.html" class="outline">View cart</a>
+                                                <a href="{{ route('cart') }}" class="outline">View cart</a>
                                                 <a href="shop-checkout.html">Checkout</a>
                                             </div>
                                         </div>
@@ -177,6 +173,11 @@
     </div>
     <!--End header-->
     <main class="main">
+            @if(session('success'))
+            <div class="alert alert-success">
+            {{ session('success') }}
+            </div> 
+        @endif
         @yield('content')
     </main>
     <!-- Preloader Start -->
